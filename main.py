7c0a3a318318cf52c7e696ca841aab198134a6a7
kennneth1 <huang.kenneth.98@gmail.com>
2023-06-16 13:16:30 -0700

    add model files

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+app = Flask(__name__)
+
+# Load the pretrained DialoGPT model and tokenizer
+model_name = "microsoft/DialoGPT-medium"
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Define a route for generating responses
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    # Get the input message from the request
+    input_message = request.json["message"]
+
+    # Tokenize the input message
+    input_ids = tokenizer.encode(input_message, return_tensors="pt")
+
+    # Generate a response from the model
+    output = model.generate(input_ids, max_length=100)
+
+    # Decode the generated response
+    response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    # Print the response as JSON
+    print({"response": response})
+
+    # Return the response as JSON
+    return jsonify({"response": response})
+
+# Run the Flask application
+if __name__ == "__main__":
+    app.run()
