b99982979d39d272974f0f88e0580ee038390cb1
kennneth1 <huang.kenneth.98@gmail.com>
2023-06-19 14:39:32 -0700

    creatate endpoint for dialogpt

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, render_template
-from utils.preprocessing import clean_response, load_model, chat
+from utils.custom_preprocessing import clean_response, load_model, chat
 import os
 
 app = Flask(__name__)
-
-# cold load the model and tokenizer
-model, tokenizer = load_model()
 
 templates_dir = os.path.join(os.getcwd(), "app", "templates")
 app.template_folder = templates_dir
@@ -14,16 +11,15 @@ app.template_folder = templates_dir
 def index():
     return render_template("index.html")
     
-# Handle POST requests to the custom chatbot endpoint
-@app.route("/api/custom-endpoint", methods=["POST"])
-def chatbot():
+# Handle POST requests to gpt2 endpoint
+@app.route("/api/gpt2-chatbot", methods=["POST"])
+def gpt2_chatbot():
     # Get the input message from the request
-    
     input_message = request.json["message"]
     print("Received message:", input_message)
 
     # Return response
-    response = chat(input_message)
+    response = chat(input_message, "gpt2-medium")
     response = response.get("response", "")  # Extract the "response" value
     print("Generated response:", response)
 
@@ -32,10 +28,22 @@ def chatbot():
 
     return response
 
-# Handle POST requests to hugging face's inference endpoint
-@app.route("/api/hugging-endpoint", methods=["POST"])
-def chatbot_with_hugging():
-    return "404: Not yet implemented"
+# Handle POST requests to dialo endpoint
+@app.route("/api/dialo-chatbot", methods=["POST"])
+def dialo_chatbot():
+    # Get the input message from the request
+    input_message = request.json["message"]
+    print("Received message:", input_message)
+
+    # Return response
+    response = chat(input_message, "microsoft/DialoGPT-medium")
+    response = response.get("response", "")  # Extract the "response" value
+    print("Generated response:", response)
+
+    response = clean_response(response, input_message)
+    print("Cleaned response:", response)
+
+    return response
 
 # Run the Flask application
 if __name__ == "__main__":
